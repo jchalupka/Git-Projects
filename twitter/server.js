4c042c2b2987bd69a760cd18e662dccf64a287d5
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2017-01-24 11:38:18 -0500

    updated the front end and added a twitter stream

--- twitter/server.js
+++ twitter/server.js
@@ -4,46 +4,73 @@ var Twit = require('twit'),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
-    io = require('socket.io').listen(server);
-    //bot = require('./public/js/twitBot.js');
-
+    io = require('socket.io').listen(server),
+    limit = require('simple-rate-limiter');
+    
 server.listen(process.env.PORT || 8080);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/front_end'));
+
 
 io.sockets.on('connect', function(socket) {
-    socket.on('start tweets', function() {
+    console.log('connected')
+    socket.emit('connected');
+
+
+    socket.on('start tweets', function(search) {
+        console.log(search);
+        
         var T = new Twit(credentials);
 
-        var stream = T.stream('statuses/filter', {'track':'google'});
+        var world = '-180,-90,180,90';
 
-        stream.on('tweet', function (tweet) {
-            //console.log(tweet);
+        var options = {
+            locations: world,
+            filter_level: 'none'
+        }
+
+        var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
+        if(search == '') {
+            options.locations = world
+        }
+
+       if(search != '') {
+            delete options.locations;
+            options.track = search;
+        }
+
+        var stream = T.stream('statuses/filter', options);
+       console.log(options);
+
+        var tick = function(tweet) {
+            streamTweets({text:tweet.text, name:tweet.user.name, username:tweet.user.screen_name, icon:tweet.user.profile_image_url});
             if(tweet.coordinates) {
                 if (tweet.coordinates != null) {
-                    console.log('tweet: ', tweet.text);
                     var locationObj = {
-                        "lat": tweet.coordinates.coordinates[0],
-                        "lng": tweet.coordinates.coordinates[1],
-                        "text": tweet.text
+                        text:tweet.text, 
+                        name:tweet.user.name, 
+                        username:tweet.user.screen_name, 
+                        icon:tweet.user.profile_image_url,
+                        lat: tweet.coordinates.coordinates[0],
+                        lng: tweet.coordinates.coordinates[1],
+                        text: tweet.text
                     }
-                    socket.broadcast.emit('twitter-stream', locationObj);
-                    socket.emit('twitter-stream', locationObj);
+                    socket.emit('location-stream', locationObj);
                 }
             }
-            else if(tweet.user) {
-                if (tweet.user.locations != null) {
-                    console.log('tweet with close location', tweet.locations);
-                }
-            }
-            else if(tweet.place) {
-                console.log('got the place');
-            }
+        }
+
+        stream.on('tweet', function (tweet) {            
+            tick(tweet);
         });
-        console.log('connected')
-        socket.emit('connected');
+
     });
+    let streamTweets = limit(function(options) {
+        socket.emit('stream', options);
+    }).to(1).per(2000);
 });
+
+
 
 
 
